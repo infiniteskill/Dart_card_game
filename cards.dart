e645a454ee2567b2d89e237ffedb62ec7176cafa
infiniteskill <rajneeshverma55@gmail.com>
2020-03-15 18:13:30 +0530

    game

---
+++ cards.dart
@@ -0,0 +1,59 @@
+void main(){
+  var deck=new Deck();
+deck.removeCard('Diamonds','Ace');
+  print(deck);
+}
+
+
+class Deck {
+  List<Card> cards=[];
+
+  Deck() {
+    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+    
+    for (var suit in suits){
+      for (var rank in ranks){
+        var card=new Card(rank:rank,suit:suit);
+        cards.add(card);
+      }
+    }
+  }
+ 
+ toString(){
+    cards.shuffle();
+   return cards.toString();
+ }
+  cardsWithSuit(String suit){
+    return  cards.where((card)=>
+       card.suit==suit
+    );
+  }
+  
+  
+  deal(int handSize){
+    var hand=cards.sublist(0,handSize);
+    cards=cards.sublist(handSize);
+    return hand;
+  }
+  
+  removeCard(String suit,String rank){
+    cards.removeWhere((card){
+      return card.suit==suit && card.rank==rank;
+    });
+  }
+}
+
+class Card {  
+  String suit;
+  String rank;
+  Card({this.rank,this.suit}){
+    
+  }
+  
+  toString(){
+   return '$rank of $suit';
+ }
+  
+}
+
